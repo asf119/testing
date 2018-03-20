bc66e8b7d02e11a6921714bf7dbe0fb22916af55
project <project@email.com>
2018-03-20 21:43:17 +0700

    fix navbar active link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { AboutComponent }   from './about/about.component';
 
 
 const routes: Routes = [
-  { path: '', component:HomeComponent, pathMatch: 'full' },
+  { path: '', redirectTo:'/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },  
   { path: 'about', component: AboutComponent },
 ];
 
